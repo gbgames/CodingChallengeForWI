ccc1d0e1500794567778839dd1206f2009bfafa9
Gianfranco Berardi <gberardi@gbgames.com>
2015-05-17 14:53:50 -0500

    Can now extract events from multiple people.

--- source/proj/EventExtractor.cpp
+++ source/proj/EventExtractor.cpp
@@ -13,9 +13,12 @@ EventExtractor::~EventExtractor()
 ImportantEvents EventExtractor::convert(const PeopleDataCollection & people)
 {
 	ImportantEvents events;
-	PeopleData person = people.at(0);
-	events.push_back(ImportantEvent(person.birthYear, BIRTH_EVENT, person.id));
-	events.push_back(ImportantEvent(person.deathYear, DEATH_EVENT, person.id));
+	for (PeopleDataCollection::const_iterator iter = people.begin(); iter != people.end(); ++iter)
+	{
+		const PeopleData & person = *(iter);
+		events.push_back(ImportantEvent(person.birthYear, BIRTH_EVENT, person.id));
+		events.push_back(ImportantEvent(person.deathYear, DEATH_EVENT, person.id));
+	}
 
 	return events;
 }
--- source/tests/projtests/TestEventExtractor.cpp
+++ source/tests/projtests/TestEventExtractor.cpp
@@ -8,6 +8,8 @@ using namespace ::testing;
 namespace 
 {
 	PersonID id1(1);
+	PersonID id2(2);
+	PersonID id3(3);
 }
 
 TEST(EventExtractor, CreatesBirthAndDeathEventsFromASinglePerson)
@@ -25,4 +27,37 @@ TEST(EventExtractor, CreatesBirthAndDeathEventsFromASinglePerson)
 	EXPECT_THAT(events.at(1).id, Eq(id1));
 	EXPECT_THAT(events.at(1).year, Eq(1961));
 	EXPECT_THAT(events.at(1).eventType, Eq(DEATH_EVENT));
+}
+
+TEST(EventExtractor, CreatesEventsFromMultiplePeople)
+{
+	PeopleDataCollection people;
+	people.push_back(PeopleData("Mr. Sanders", 1926, 1961, id1));
+	people.push_back(PeopleData("A. A. Milne", 1900, 1956, id2));
+	people.push_back(PeopleData("Julie Andrews", 1935, 2000, id3));
+
+	EventExtractor extractor;
+	ImportantEvents events = extractor.convert(people);
+
+	ASSERT_THAT(events.size(), Eq(6));
+	EXPECT_THAT(events.at(0).id, Eq(id1));
+	EXPECT_THAT(events.at(0).year, Eq(1926));
+	EXPECT_THAT(events.at(0).eventType, Eq(BIRTH_EVENT));
+	EXPECT_THAT(events.at(1).id, Eq(id1));
+	EXPECT_THAT(events.at(1).year, Eq(1961));
+	EXPECT_THAT(events.at(1).eventType, Eq(DEATH_EVENT));
+
+	EXPECT_THAT(events.at(2).id, Eq(id2));
+	EXPECT_THAT(events.at(2).year, Eq(1900));
+	EXPECT_THAT(events.at(2).eventType, Eq(BIRTH_EVENT));
+	EXPECT_THAT(events.at(3).id, Eq(id2));
+	EXPECT_THAT(events.at(3).year, Eq(1956));
+	EXPECT_THAT(events.at(3).eventType, Eq(DEATH_EVENT));
+
+	EXPECT_THAT(events.at(4).id, Eq(id3));
+	EXPECT_THAT(events.at(4).year, Eq(1935));
+	EXPECT_THAT(events.at(4).eventType, Eq(BIRTH_EVENT));
+	EXPECT_THAT(events.at(5).id, Eq(id3));
+	EXPECT_THAT(events.at(5).year, Eq(2000));
+	EXPECT_THAT(events.at(5).eventType, Eq(DEATH_EVENT));
 }
