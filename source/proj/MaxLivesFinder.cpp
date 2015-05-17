bd48635c0c9e7754fb2a4bd63d237e64e716e0a0
Gianfranco Berardi <gberardi@gbgames.com>
2015-05-17 12:35:01 -0500

    Can now report on two living people in a single year.

--- source/proj/MaxLivesFinder.cpp
+++ source/proj/MaxLivesFinder.cpp
@@ -11,8 +11,11 @@ MaxLivesFinder::~MaxLivesFinder()
 MaxLivesResult MaxLivesFinder::searchEvents(const ImportantEvents & events)
 {
 	MaxLivesResult result;
-	result.earliestYear = events.at(0).year;
-	result.idCollection.push_back(events.at(0).id);
+	for (ImportantEvents::const_iterator iter = events.begin(); iter != events.end(); ++iter)
+	{
+		result.earliestYear = iter->year;
+		result.idCollection.push_back(iter->id);
+	}
 
 	return result;
 }
--- source/tests/projtests/TestMaxLivesFinder.cpp
+++ source/tests/projtests/TestMaxLivesFinder.cpp
@@ -8,6 +8,7 @@ using namespace ::testing;
 namespace 
 {
 	PersonID id1(1);
+	PersonID id2(2);
 }
 
 TEST(MaxLivesFinder, GivenASingleBirthEventReturnYearAndIDOfEvent)
@@ -20,4 +21,18 @@ TEST(MaxLivesFinder, GivenASingleBirthEventReturnYearAndIDOfEvent)
 	EXPECT_THAT(result.earliestYear, Eq(1999));
 	ASSERT_THAT(result.idCollection.size(), Eq(1));
 	EXPECT_THAT(result.idCollection.at(0), Eq(id1));
+}
+
+TEST(MaxLivesFinder, GivenTwoBirthEventsReturnYearInWhichBothIDsAreAlive)
+{
+	ImportantEvents events;
+	events.push_back(ImportantEvent(1999, BIRTH_EVENT, id1));
+	events.push_back(ImportantEvent(2000, BIRTH_EVENT, id2));
+	MaxLivesFinder finder;
+	MaxLivesResult result = finder.searchEvents(events);
+
+	EXPECT_THAT(result.earliestYear, Eq(2000));
+	ASSERT_THAT(result.idCollection.size(), Eq(2));
+	EXPECT_THAT(result.idCollection.at(0), Eq(id1));
+	EXPECT_THAT(result.idCollection.at(1), Eq(id2));
 }
